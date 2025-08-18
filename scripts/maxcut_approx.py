a5e329587fdf1a9f7647b662b91e03e36fc7d9ac
WrathfulSpatula <stranoj@gmail.com>
2025-08-18 17:27:57 -0400

    Option of solver in maxcut_approx.py

--- scripts/maxcut_approx.py
+++ scripts/maxcut_approx.py
@@ -7,7 +7,7 @@
 
 import networkx as nx
 import numpy as np
-from PyQrackIsing import spin_glass_solver
+from PyQrackIsing import maxcut_tfim, spin_glass_solver
 
 
 # NP-complete spin glass
@@ -58,6 +58,7 @@ if __name__ == "__main__":
     # NP-complete spin glass
     # G = generate_spin_glass_graph(seed=42)
 
-    cut_value, bitstring, cut_edges, energy = spin_glass_solver(G)
+    cut_value, bitstring, cut_edges = maxcut_tfim(G)
+    # cut_value, bitstring, cut_edges, energy = spin_glass_solver(G)
 
     print((cut_value, bitstring, cut_edges))
