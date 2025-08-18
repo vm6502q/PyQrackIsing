Fix sign convention

PyQrackIsing/maxcut_tfim.py
@@ -89,9 +89,9 @@ def maxcut_tfim(
 
     J_eff = None
     if is_spin_glass:
-        J_eff = np.array([sum(-edge_attributes.get('weight', 1.0) for _, edge_attributes in G.adj[i].items()) for i in range(n_qubits)], dtype=np.float64)
-    else:
         J_eff = np.array([sum(edge_attributes.get('weight', 1.0) for _, edge_attributes in G.adj[i].items()) for i in range(n_qubits)], dtype=np.float64)
+    else:
+        J_eff = np.array([sum(-edge_attributes.get('weight', 1.0) for _, edge_attributes in G.adj[i].items()) for i in range(n_qubits)], dtype=np.float64)
     degrees = np.array([sum(abs(edge_attributes.get('weight', 1.0)) for _, edge_attributes in G.adj[i].items()) for i in range(n_qubits)], dtype=np.float64)
     thresholds = tfim_sampler._maxcut_hamming_cdf(J_eff, degrees, quality)
     G_dict = nx.to_dict_of_lists(G)