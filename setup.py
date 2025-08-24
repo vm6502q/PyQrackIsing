ba9fa6a4a02450ca2a2e16e3b56bd5a0e2af30fc
WrathfulSpatula <stranoj@gmail.com>
2025-08-24 01:55:34 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ ext_modules = [CMakeExtension('tfim_sampler')]
 
 setup(
     name='PyQrackIsing',
-    version='1.11.1',
+    version='1.12.0',
     author='Dan Strano',
     author_email='stranoj@gmail.com',
     description='Near-ideal closed-form solutions for transverse field Ising model (TFIM)',
