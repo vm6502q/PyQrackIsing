e4797d727ef1fe62f8d0a2665d592f584091140d
WrathfulSpatula <stranoj@gmail.com>
2025-08-26 17:49:23 -0400

    Better quality defaults

--- PyQrackIsing/maxcut_tfim.py
+++ PyQrackIsing/maxcut_tfim.py
@@ -102,8 +102,8 @@ def maxcut_hamming_cdf(n_qubits, J_func, degrees, quality, hamming_prob):
 
     n_steps = 1 << quality
     delta_t = 1.0 / n_steps
-    tot_t = n_steps * delta_t
-    h_mult = 32.0 / tot_t
+    tot_t = 64.0 * n_steps * delta_t
+    h_mult = 64.0 / tot_t
     n_bias = n_qubits - 1
 
     theta = np.zeros(n_qubits)
@@ -351,7 +351,7 @@ def maxcut_tfim(
     group_size = n_qubits - 1
 
     if quality is None:
-        quality = 10
+        quality = 9
 
     if shots is None:
         # Number of measurement shots
@@ -368,8 +368,8 @@ def maxcut_tfim(
 
     if IS_CUDA_AVAILABLE and cuda.is_available() and grid_size >= 128:
         delta_t = 1.0 / n_steps
-        tot_t = n_steps * delta_t
-        h_mult = 32.0 / tot_t
+        tot_t = 64.0 * n_steps * delta_t
+        h_mult = 64.0 / tot_t
         theta = init_theta(delta_t, tot_t, h_mult, n_qubits, J_eff, degrees)
 
         cuda_maxcut_hamming_cdf[grid_dims, group_size](delta_t, tot_t, h_mult, J_eff, degrees, theta, thresholds)
--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ ext_modules = [CMakeExtension('tfim_sampler')]
 
 setup(
     name='PyQrackIsing',
-    version='1.15.0',
+    version='1.16.0',
     author='Dan Strano',
     author_email='stranoj@gmail.com',
     description='Near-ideal closed-form solutions for transverse field Ising model (TFIM)',
