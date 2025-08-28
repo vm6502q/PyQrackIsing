d145bbcd13a4ad9716df030c78f5a7c52207874e
WrathfulSpatula <stranoj@gmail.com>
2025-08-28 17:11:54 -0400

    Debug

--- PyQrackIsing/tsp_symmetric.py
+++ PyQrackIsing/tsp_symmetric.py
@@ -192,8 +192,8 @@ def tsp_symmetric(G, quality=1, shots=None, correction_quality=2, monte_carlo=Fa
                 continue
             G_b[i, j] = G_m[b[i], b[j]]
 
-    sol_a = tsp_symmetric(G_a, quality=quality, correction_quality=correction_quality, is_cyclic=False, is_2_opt=False, is_3_opt=False, multi_start=multi_start)
-    sol_b = tsp_symmetric(G_b, quality=quality, correction_quality=correction_quality, is_cyclic=False, is_2_opt=False, is_3_opt=False, multi_start=multi_start)
+    sol_a = tsp_symmetric(G_a, quality=quality, correction_quality=correction_quality, monte_carlo=monte_carlo, is_cyclic=False, is_2_opt=False, is_3_opt=False, multi_start=multi_start)
+    sol_b = tsp_symmetric(G_b, quality=quality, correction_quality=correction_quality, monte_carlo=monte_carlo, is_cyclic=False, is_2_opt=False, is_3_opt=False, multi_start=multi_start)
 
     path_a = [a[x] for x in sol_a[0]]
     path_b = [b[x] for x in sol_b[0]]
