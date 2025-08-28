285fa484222f746fefd5314241c48a3d36a9f886
WrathfulSpatula <stranoj@gmail.com>
2025-08-28 10:22:40 -0400

    Always cyclic for 2-opts and 3-opts

--- PyQrackIsing/tsp_symmetric.py
+++ PyQrackIsing/tsp_symmetric.py
@@ -242,22 +242,17 @@ def tsp_symmetric(G, quality=1, shots=None, correction_quality=2, is_3_opt=True,
             path_a, path_b = path_b, path_a
             terminals_a, terminals_b = terminals_b, terminals_a
 
-    cycle_node = None
-    if is_cyclic:
-        cycle_node = best_path[0]
-        best_path += [cycle_node]
+    cycle_node = best_path[0]
+    best_path += [cycle_node]
 
     best_path, best_weight = two_opt(best_path, G_m)
 
     if is_3_opt:
         best_path, best_weight = targeted_three_opt(best_path, G_m, k_neighbors)
  
-    if is_cyclic:
+    if not is_cyclic:
         cycle_index = best_path.index(cycle_node)
         best_weight -= G_m[best_path[cycle_index], best_path[cycle_index + 1]]
         best_path = best_path[cycle_index + 1:] + best_path[:cycle_index]
-
-        best_weight += G_m[best_path[-1], best_path[0]]
-        best_path += [best_path[0]]
 
     return [nodes[x] for x in best_path], best_weight
