b8108276ed47cc6916c8c78ff11598cce322270a
WrathfulSpatula <stranoj@gmail.com>
2025-09-09 13:54:53 -0400

    Improve halting_heuristic.py

--- scripts/halting_heuristic.py
+++ scripts/halting_heuristic.py
@@ -1,6 +1,9 @@
+import math
 import networkx as nx
 import numpy as np
 import time
+
+from collections import Counter
 
 from PyQrackIsing import spin_glass_solver
 
@@ -23,24 +26,25 @@ def build_program_graph(halts=True, size=7):
         G.add_edge(nodes[-1], nodes[0], weight=2)
     return G
 
-def test_halting(G):
-    _, _, cut, _ = spin_glass_solver(G)
-    print(cut)
-    cut = (set(cut[0]), set(cut[1]))
-    is_halting = True
-    for i in range(3):
-        _, _, n_cut, _ = spin_glass_solver(G, quality=3, correction_quality=3)
-        print(n_cut)
-        n_cut = (set(n_cut[0]), set(n_cut[1]))
-        if cut[0] != n_cut[0] and cut[0] != n_cut[1]:
-            is_halting = False
-            break
+def halting_entropy(G, trials=10):
+    cuts = []
+    for i in range(trials):
+        _, _, cut, _ = spin_glass_solver(G, quality=3, correction_quality=3)
+        cut = (frozenset(cut[0]), frozenset(cut[1])) if 0 in cut[0] else (frozenset(cut[1]), frozenset(cut[0]))
+        cuts.append(cut)
 
-    return is_halting
+    counts = Counter(cuts)
+    probs = [c / trials for c in counts.values()]
+    entropy = -sum(p * math.log2(p) for p in probs)
+
+    return entropy, counts
 
 print("Graph that actually halts:")
 halts = build_program_graph(halts=True)
-if test_halting(halts):
+entropy, counts = halting_entropy(halts)
+print(f"Entropy: {entropy}")
+print(f"Counts: {counts}")
+if np.isclose(entropy, 0):
     print("Probably halts.")
 else:
     print("Probably does not halt.")
@@ -49,7 +53,10 @@ print()
 
 print("Graph that actually doesn't halt:")
 loops = build_program_graph(halts=False)
-if test_halting(loops):
+entropy, counts = halting_entropy(loops)
+print(f"Entropy: {entropy}")
+print(f"Counts: {counts}")
+if np.isclose(entropy, 0):
     print("Probably halts.")
 else:
     print("Probably does not halt.")
