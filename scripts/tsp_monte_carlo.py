ecdea6ba370f7198ca8f5f2b117beb089b49f9e4
WrathfulSpatula <stranoj@gmail.com>
2025-09-16 17:07:18 -0400

    Shared memory for TSP

--- scripts/tsp_monte_carlo.py
+++ scripts/tsp_monte_carlo.py
@@ -2,6 +2,7 @@
 # Produced by Dan Strano, Elara (the OpenAI custom GPT)
 
 from pyqrackising import tsp_symmetric
+from multiprocessing import shared_memory
 import multiprocessing
 import networkx as nx
 import numpy as np
@@ -21,7 +22,10 @@ def generate_tsp_graph(n_nodes=64, seed=None):
 
 
 def bootstrap_worker(args):
-    return tsp_symmetric(G=args[0], monte_carlo=True, k_neighbors=args[1])
+    path, length = tsp_symmetric(G=args[0], monte_carlo=True, k_neighbors=args[1])
+    args[2].close()
+
+    return path, length
 
 
 if __name__ == "__main__":
@@ -32,11 +36,16 @@ if __name__ == "__main__":
     seed = int(sys.argv[4]) if len(sys.argv) > 4 else None
 
     G = generate_tsp_graph(n_nodes=n_nodes, seed=seed)
-    G_m = nx.to_numpy_array(G, weight='weight', nonedge=0.0)
+    _G_m = nx.to_numpy_array(G, weight='weight', nonedge=0.0)
+    shm = shared_memory.SharedMemory(create=True, size=_G_m.nbytes)
+    G_m = np.ndarray(_G_m.shape, dtype=_G_m.dtype, buffer=shm.buf)
+    G_m[:] = _G_m[:]
+    _G_m = None
+    G = None
 
     args = []
     for i in range(multi_start):
-        args.append((G_m, k_neighbors))
+        args.append((G_m, k_neighbors, shm))
     with multiprocessing.Pool(processes=multi_start) as pool:
         results = pool.map(bootstrap_worker, args)
 
@@ -47,7 +56,10 @@ if __name__ == "__main__":
     reconstructed_path_length = 0
     best_nodes = None
     for i in range(len(best_circuit) - 1):
-        reconstructed_path_length += G[best_circuit[i]][best_circuit[i + 1]]["weight"]
+        reconstructed_path_length += G_m[best_circuit[i]][best_circuit[i + 1]]
+
+    G_m = None
+    shm.unlink()
 
     print(f"Random seed: {seed}")
     print(f"Path: {best_circuit}")
