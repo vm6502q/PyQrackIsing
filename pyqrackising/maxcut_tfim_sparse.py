9a86406a48aa9b5843479b7a2a01a5bc573560e1
WrathfulSpatula <stranoj@gmail.com>
2025-09-22 22:33:15 -0400

    DRY

--- pyqrackising/maxcut_tfim_sparse.py
+++ pyqrackising/maxcut_tfim_sparse.py
@@ -4,96 +4,13 @@ import numpy as np
 import os
 from numba import njit, prange
 
-from .maxcut_tfim_util import opencl_context
+from .maxcut_tfim_util import init_thresholds, maxcut_hamming_cdf, opencl_context, probability_by_hamming_weight
 
 IS_OPENCL_AVAILABLE = True
 try:
     import pyopencl as cl
 except ImportError:
     IS_OPENCL_AVAILABLE = False
-
-
-@njit
-def probability_by_hamming_weight(J, h, z, theta, t, n_qubits):
-    bias = np.empty(n_qubits - 1, dtype=np.float32)
-
-    # critical angle
-    theta_c = np.arcsin(max(-1.0, min(1.0, abs(h) / (z * J))))
-
-    p = (
-        pow(2.0, abs(J / h) - 1.0)
-        * (1.0 + np.sin(theta - theta_c) * np.cos(1.5 * np.pi * J * t + theta) / (1.0 + np.sqrt(t)))
-        - 0.5
-    )
-
-    if (p * n_qubits) >= 1024:
-        return bias
-
-    tot_n = 1.0 + 1.0 / pow(2.0, p * n_qubits)
-    factor = pow(2.0, -p)
-    n = 1.0
-    for q in range(1, n_qubits):
-        n *= factor
-        bias[q - 1] = n
-        tot_n += n
-    bias /= tot_n
-
-    if J > 0.0:
-        return bias[::-1]
-
-    return bias
-
-
-@njit(parallel=True)
-def maxcut_hamming_cdf(n_qubits, J_func, degrees, quality, hamming_prob):
-    if n_qubits < 2:
-        hamming_prob.fill(0.0)
-        return
-
-    n_steps = 1 << quality
-    delta_t = 1.0 / n_steps
-    tot_t = 2.0 * n_steps * delta_t
-    h_mult = 2.0 / tot_t
-    n_bias = n_qubits - 1
-
-    theta = np.empty(n_qubits, dtype=np.float32)
-    for q in range(n_qubits):
-        J = J_func[q]
-        z = degrees[q]
-        theta[q] = np.arcsin(
-            max(
-                -1.0,
-                min(
-                    1.0,
-                    (1.0 if J > 0.0 else -1.0) if np.isclose(abs(z * J), 0.0) else (abs(h_mult) / (z * J)),
-                ),
-            )
-        )
-
-    for qc in prange(n_qubits, n_steps * n_qubits):
-        step = qc // n_qubits
-        q = qc % n_qubits
-        J_eff = J_func[q]
-        if np.isclose(abs(J_eff), 0.0):
-            continue
-        z = degrees[q]
-        theta_eff = theta[q]
-        t = step * delta_t
-        tm1 = (step - 1) * delta_t
-        h_t = h_mult * (tot_t - t)
-        bias = probability_by_hamming_weight(J_eff, h_t, z, theta_eff, t, n_qubits)
-        last_bias = probability_by_hamming_weight(J_eff, h_t, z, theta_eff, tm1, n_qubits)
-        for i in range(n_bias):
-            hamming_prob[i] += bias[i] - last_bias[i]
-
-    tot_prob = hamming_prob.sum()
-    hamming_prob /= tot_prob
-
-    tot_prob = 0.0
-    for i in range(n_bias):
-        tot_prob += hamming_prob[i]
-        hamming_prob[i] = tot_prob
-    hamming_prob[-1] = 2.0
 
 
 @njit
@@ -257,27 +174,6 @@ def init_J_and_z(G_data, G_rows, G_cols):
     J_eff /= J_max
 
     return J_eff, degrees
-
-
-@njit
-def init_thresholds(n_qubits):
-    n_bias = n_qubits - 1
-    thresholds = np.empty(n_bias, dtype=np.float32)
-    tot_prob = 0
-    p = 1.0
-    if n_qubits & 1:
-        q = n_qubits // 2
-        thresholds[q - 1] = p
-        tot_prob = p
-        p /= 2
-    for q in range(1, n_qubits // 2):
-        thresholds[q - 1] = p
-        thresholds[n_bias - q] = p
-        tot_prob += 2 * p
-        p /= 2
-    thresholds /= tot_prob
-
-    return thresholds
 
 
 @njit(parallel=True)
