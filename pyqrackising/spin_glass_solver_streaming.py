Fix off-by-one

pyqrackising/spin_glass_solver_streaming.py
@@ -141,7 +141,6 @@ def spin_glass_solver_streaming(
                 if n_qubits < k:
                     break
 
-                combos = []
                 if len(combos_list) < k:
                     combos = np.array(list(
                         item for sublist in itertools.combinations(range(n_qubits), k) for item in sublist
@@ -171,7 +170,7 @@ def spin_glass_solver_streaming(
             best_theta = reheat_theta.copy()
             min_energy = reheat_min_energy
 
-        if reheat_round < reheat_tries:
+        if reheat_round <= reheat_tries:
             num_to_flip = int(np.round(np.log2(n_qubits)))
             bits_to_flip = random.sample(list(range(n_qubits)), num_to_flip)
             for bit in bits_to_flip: