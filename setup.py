37c48a7957301f34adc155d966779fbc988ce042
WrathfulSpatula <stranoj@gmail.com>
2025-10-29 10:48:03 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(README_PATH) as readme_file:
 
 setup(
     name='pyqrackising',
-    version='9.5.5',
+    version='9.5.6',
     author='Dan Strano',
     author_email='stranoj@gmail.com',
     description='Fast MAXCUT, TSP, and sampling heuristics from near-ideal transverse field Ising model (TFIM)',
