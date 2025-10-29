Fix off-by-one

pyqrackising/otoc.py
@@ -52,7 +52,7 @@ def get_otoc_hamming_distribution(J=-1.0, h=2.0, z=4, theta=0.0, t=5, n_qubits=6
     # Normalize:
     diff_z /= diff_z.sum()
 
-    entropy_frac = np.atan(2 ** (entropy_frac / math.sqrt(n_qubits))) * 2 / np.pi
+    entropy_frac = np.atan(2 ** (entropy_frac / math.sqrt(n_qubits)) - 1) * 2 / np.pi
     max_entropy = np.empty(n_bias, dtype=np.float64)
     tot_prob = 0
     p = 1.0