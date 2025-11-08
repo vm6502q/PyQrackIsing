ba84119f203fb361744dfc3c6e69f3e146195846
WrathfulSpatula <stranoj@gmail.com>
2025-11-08 11:20:27 -0500

    Fix LABS argv

--- scripts/labs.py
+++ scripts/labs.py
@@ -81,8 +81,8 @@ def generate_labs_qubo(N, lam=10.0):
 
 
 if __name__ == "__main__":
-    n_nodes = int(sys.argv[2]) if len(sys.argv) > 2 else 32
-    quality = int(sys.argv[3]) if len(sys.argv) > 3 else None
+    n_nodes = int(sys.argv[1]) if len(sys.argv) > 1 else 32
+    quality = int(sys.argv[2]) if len(sys.argv) > 2 else None
     G_m, labels = generate_labs_qubo(n_nodes)
     best_bitstring, best_cut_value, best_cut, best_energy = spin_glass_solver(G_m, quality=quality, is_spin_glass=False)
 
