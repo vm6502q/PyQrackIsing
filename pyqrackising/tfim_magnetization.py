5173ceb0e2fe31a8581d59580e1845cbf4a67521
WrathfulSpatula <stranoj@gmail.com>
2025-11-12 10:39:59 -0500

    Debug

--- pyqrackising/tfim_magnetization.py
+++ pyqrackising/tfim_magnetization.py
@@ -1,5 +1,8 @@
-from .maxcut_tfim_util import probability_by_hamming_weight
+from .maxcut_tfim_util import probability_by_hamming_weight, opencl_context
 from numba import njit
+
+
+epsilon = opencl_context.epsilon
 
 
 @njit
--- pyqrackising/tfim_square_magnetization.py
+++ pyqrackising/tfim_square_magnetization.py
@@ -1,5 +1,9 @@
-from .maxcut_tfim_util import probability_by_hamming_weight
+from .maxcut_tfim_util import probability_by_hamming_weight, opencl_context
 from numba import njit
+
+
+epsilon = opencl_context.epsilon
+
 
 @njit
 def tfim_square_magnetization(J=-1.0, h=2.0, z=4, theta=0.174532925199432957, t=5, n_qubits=56):
