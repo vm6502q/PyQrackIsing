37c8a2dd151866e1cbbee29f800657d535efe58f
WrathfulSpatula <stranoj@gmail.com>
2026-08-17 17:38:52 -0400

    'Cross the t'

--- pyqrackising/generate_tfim_samples.py
+++ pyqrackising/generate_tfim_samples.py
@@ -259,16 +259,22 @@ def generate_tfim_samples(J=-1.0, h=2.0, z=4, theta=0.174532925199432957, t=5, n
 omega_b, phi, tau = 4.0, -1.5, (1.0 / 3.0)
 A_peak = 15.0
 
+@njit(cache=True)
 def A_of_r(J, h, z):
     r = h / (z * J)
     return A_peak * 4 * abs(r) / (1 + abs(r))**2
 
+
+@njit(cache=True)
 def beta_formula(t, J, h, z):
     return A_of_r(J, h, z) * np.sin(omega_b * J * t + phi) / (1.0 + (t / tau))
 
+
+@njit(cache=True)
 def tilt(bias, beta, n_qubits):
     w = bias * np.exp(beta * np.arange(n_qubits + 1))
     return w / w.sum()
+
 
 def sample_from_bias(bias, shots, n_qubits):
     n_rows, n_cols = factor_width(n_qubits)
@@ -285,6 +291,7 @@ def sample_from_bias(bias, shots, n_qubits):
         samples += sample_fixed_hamming_weight(hw, counts[hw], n_rows, n_cols)
     np.random.shuffle(samples)
     return samples
+
 
 def generate_fermi_hubbard_samples(J=-1.0, h=2.0, z=4, theta=0.174532925199432957, t=5, n_qubits=56, shots=100, omega=1.5 * np.pi):
     z_bias = get_tfim_hamming_distribution(J=J, h=h, z=z, theta=theta, t=t, n_qubits=n_qubits)
