5954413af4039a8fe0e3e17242d7ed4a3e948bdb
WrathfulSpatula <stranoj@gmail.com>
2026-09-08 18:22:52 -0400

    Debug

--- pyqrackising/otoc.py
+++ pyqrackising/otoc.py
@@ -15,19 +15,6 @@ def get_otoc_hamming_distribution(J=-1.0, h=2.0, z=4, theta=0.0, t=5, n_qubits=6
         bias[0] = 1.0
         return bias
 
-    diff_x = np.empty(n_bias, dtype=np.float64)
-    tot_prob = 0
-    p = 1.0
-    for q in range(n_qubits >> 1):
-        diff_x[q] = p
-        diff_x[n_bias - (q + 1)] = p
-        tot_prob += 2 * p
-        p = math.comb(n_qubits, q + 1)
-    if n_qubits & 1:
-        diff_x[n_qubits >> 1] = p
-        tot_prob += p
-    diff_x *= n_qubits / tot_prob
-
     diff_z = np.zeros(n_bias, dtype=np.float64)
     diff_z[0] = 1.0
     for pauli_string in pauli_strings:
@@ -40,26 +27,29 @@ def get_otoc_hamming_distribution(J=-1.0, h=2.0, z=4, theta=0.0, t=5, n_qubits=6
             continue
         signal_frac / n_qubits
 
+        diff_x = np.zeros(n_bias, dtype=np.float64)
+
+        fwd = probability_by_hamming_weight(J, h, z, theta, t, n_qubits + 1)
+        rev = probability_by_hamming_weight(-J, -h, z, theta + np.pi, t, n_qubits + 1)
+        diff_theta = rev - fwd
+
         phi = theta + np.pi / 2
         fwd = probability_by_hamming_weight(h, J, z, phi, t, n_qubits + 1)
         rev = probability_by_hamming_weight(-h, -J, z, phi - np.pi, t, n_qubits + 1)
         diff_phi = rev - fwd
 
-        fwd = probability_by_hamming_weight(-J, -h, z, theta, t, n_qubits + 1)
-        rev = probability_by_hamming_weight(J, h, z, theta + np.pi, t, n_qubits + 1)
-        diff_theta = rev - fwd
-
         for b in pauli_string:
             match b:
                 case "X":
-                    diff_x += diff_phi
-                case "Z":
                     diff_x += diff_theta
+                case "Z":
+                    diff_x += diff_phi
                 case "Y":
                     diff_x += diff_theta + diff_phi
                 case _:
                     pass
 
+        diff_x /= diff_x.sum()
         diff_z = signal_frac * diff_z + (1.0 - signal_frac) * diff_x
         diff_z /= diff_z.sum()
 
