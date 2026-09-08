ac46d4511a1d47b9ca0692d303850c5bb463257e
WrathfulSpatula <stranoj@gmail.com>
2026-09-08 11:40:47 -0400

    Reverse phi and theta

--- pyqrackising/otoc.py
+++ pyqrackising/otoc.py
@@ -42,6 +42,10 @@ def get_otoc_hamming_distribution(J=-1.0, h=2.0, z=4, theta=0.0, t=5, n_qubits=6
 
         signal_frac -= term_signal
 
+        fwd = probability_by_hamming_weight(J, h, z, theta, t, n_qubits + 1)
+        rev = probability_by_hamming_weight(-J, -h, z, theta + np.pi, t, n_qubits + 1)
+        diff_theta = rev - fwd
+
         phi = theta + np.pi / 2
         fwd = probability_by_hamming_weight(-h, -J, z, phi, t, n_qubits + 1)
         rev = probability_by_hamming_weight(h, J, z, phi - np.pi, t, n_qubits + 1)
@@ -52,10 +56,10 @@ def get_otoc_hamming_distribution(J=-1.0, h=2.0, z=4, theta=0.0, t=5, n_qubits=6
                 case "X":
                     diff_z += diff_phi
                 case "Z":
-                    diff_x += diff_phi
+                    diff_x += diff_theta
                 case "Y":
                     diff_z += diff_phi
-                    diff_x += diff_phi
+                    diff_x += diff_theta
                 case _:
                     pass
 
