72023a19cf724d5bcc06dfc5be38ce7e5d0b9785
WrathfulSpatula <stranoj@gmail.com>
2026-09-08 22:43:04 -0400

    Code standards

--- pyqrackising/otoc.py
+++ pyqrackising/otoc.py
@@ -55,9 +55,6 @@ def get_otoc_hamming_distribution(J=-1.0, h=2.0, z=4, theta=0.0, t=5, n_qubits=6
         if len(pauli_string) != n_qubits:
             raise ValueError("OTOCS pauli_string must be same length as n_qubits! (Use 'I' for qubits that aren't changed.)")
 
-        if pauli_string.count("I") == n_qubits:
-            continue
-
         for b in pauli_string:
             match b:
                 case "X":
